2f4bed1368cab42bd481ba62e3ff329abf08aaf6
RGCoes <sigma_fi19@yahoo.com>
2019-07-26 01:45:23 +0200

    methode toegevoegd

--- GoeiePoging.java
+++ GoeiePoging.java
@@ -1,5 +1,13 @@
+package githubexercise;
+
 public class GoeiePoging {
     public static void main(String[] args) {
         System.out.println("Gaat lukken");
     }
+
+    public void eersteMethode(){
+        System.out.println("Hallo");
+    }
+
 }
+
